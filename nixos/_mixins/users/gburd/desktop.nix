338eb8964ae0661727dc3edeed36bb1fd39af634
Greg Burd <greg@burd.me>
2024-05-24 15:13:28 -0400

    sort

--- nixos/_mixins/users/gburd/desktop.nix
+++ nixos/_mixins/users/gburd/desktop.nix
@@ -1,14 +1,14 @@
 { desktop, pkgs, lib, ... }: {
   imports = [
-    ../../desktop/chromium.nix
     ../../desktop/chromium-extensions.nix
+    ../../desktop/chromium.nix
     ../../desktop/firefox.nix
     ../../desktop/google-chrome.nix
+    ../../desktop/jetbrains-toolbox.nix
     ../../desktop/lutris.nix
     ../../desktop/spotify.nix
     ../../desktop/tilix.nix
     ../../desktop/vscode.nix
-    ../../desktop/jetbrains-toolbox.nix
   ]
   ++ lib.optional (builtins.pathExists (../.. + "/desktop/${desktop}.nix")) ../../desktop/${desktop}.nix
   ++ lib.optional (builtins.pathExists (../.. + "/desktop/${desktop}-apps.nix")) ../../desktop/${desktop}-apps.nix;
