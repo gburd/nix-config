2a543acc6710db37f22cd446cda4d7ed3b1fb802
Greg Burd <greg@burd.me>
2024-06-02 15:22:07 -0400

    change to gnome

--- nixos/workstation/floki/default.nix
+++ nixos/workstation/floki/default.nix
@@ -4,7 +4,7 @@
 # RAM:         32GB DDR5
 # SATA:        WD_BLACK SN850X 4TB (624331WD) SSD
 
-{ inputs, lib, pkgs, ... }:
+{ inputs, config, lib, pkgs, ... }:
 {
   imports = [
     (import ./disks.nix)
@@ -104,4 +104,26 @@
   services.fprintd.enable = true;
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
+
+  security.pam.services.login.fprintAuth = true;
+  # similarly to how other distributions handle the fingerprinting login
+  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+    text = ''
+      auth       required                    pam_shells.so
+      auth       requisite                   pam_nologin.so
+      auth       requisite                   pam_faillock.so      preauth
+      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      auth       optional                    pam_permit.so
+      auth       required                    pam_env.so
+      auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
+      auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+      account    include                     login
+
+      password   required                    pam_deny.so
+
+      session    include                     login
+      session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+    '';
+  };
 }
